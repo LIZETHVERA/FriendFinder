8b3e62d7a55e0b8b4aa6da63289ed71bee9d3686
LIZETHVERA <lizeth.veraro@gmail.com>
2019-05-23 18:42:20 -0700

    commit deploy pages

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 var express = require("express");
-var path = require("path");
-
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
+var htmlRoutes = require("./app/routing/htmlRoutes");
 
 var PORT = process.env.PORT || 3000;
 var app = express();
 
-
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname + "/app/public/home.html"));
-});
+app.use("/", htmlRoutes);
+app.use(express.static("app/routing"));
 
 
 app.listen(PORT, function () {
